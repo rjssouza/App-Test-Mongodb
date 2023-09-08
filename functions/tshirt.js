b7f2ddb6370ab7489be209aea871a91be6853c25
Robson Souza <robson.souza@wisout.com>
2023-09-09 00:35:19 +0100

    Atualizacao rodar local

--- functions/tshirt.js
+++ functions/tshirt.js
@@ -1,25 +1,22 @@
 const v8n = require("v8n");
 
-const validate_name = (name) => {
-    return v8n()
-        .string()
-        .minLength(3)
-        .check(name);
+const validateName = (name) => {
+  return v8n().string().minLength(3).check(name);
 };
 
 const saveTshirt = async (tshirt) => {
-    validate_name(tshirt.name);
+  validateName(tshirt.name);
+  const serviceName = context.environment.tag + "-mongodb-atlas";
+  const dbName = context.values.get("DATABASE");
+  const dbDailyReadings = "tshirt";
 
-    const serviceName = context.environment.tag +"-mongodb-atlas";
-    const  dbName = context.values.get("DATABASE");
-    const  dbDailyReadings = "tshirt";
-
-    const  collection = context.services.get(serviceName).db(dbName).collection(dbDailyReadings);
-    const resInsert = await collection.insertOne(tshirt);
-
-    const findResult = await collection.find().toArray();
-
-    return resInsert;
+  const collection = context.services
+    .get(serviceName)
+    .db(dbName)
+    .collection(dbDailyReadings);
+  const resInsert = await collection.insertOne(tshirt);
+  // const findResult = await collection.find().toArray();
+  return resInsert;
 };
 
 // Function exported to App Services
@@ -27,5 +24,5 @@ exports = saveTshirt;
 
 // export locally for use in unit test
 if (typeof module !== "undefined") {
-    module.exports = saveTshirt;
+  module.exports = saveTshirt;
 }
--- index.js
+++ index.js
@@ -1,69 +1,100 @@
-
-const _ = require('lodash');
-const process = require('process');
+const _ = require("lodash");
+const process = require("process");
 const { MongoClient } = require("mongodb");
 const realm_config = require("./realm_config.json");
-const functions = {
-    tshirt: require("./functions/tshirt"),
-    hello: require("./functions/hello")
+const _env = process.argv[2];
+const _fn = process.argv[3];
+
+if (!_env) throw new Error("Env e obrigatorio");
+if (!_fn) throw new Error("Necessário indicar função");
+
+const args =
+  process.argv.length <= 4
+    ? []
+    : _(process.argv)
+        .drop(3)
+        .take(process.argv.length)
+        .value()
+        .map(function (v) {
+          try {
+            return JSON.parse(v);
+          } catch (err) {
+            return v;
+          }
+        });
+
+const execute = async (fn, ...argArray) => {
+  var body = `global.context.functions.${fn}(...arguments);`;
+  var func = new Function(body);
+  func.call(...argArray);
 };
 
-const env = process.argv[2];
-const { values } = env == "local" ? require(`./environments/no-environment.json`) : require(`./environments/${env}.json`);
+const functions = {
+  tshirt: require("./functions/tshirt"),
+  hello: require("./functions/hello"),
+  execute,
+};
+
+const { values } =
+  _env == "local"
+    ? require(`./environments/no-environment.json`)
+    : require(`./environments/${_env}.json`);
 
 const services = [
-    {
-        "nome": "DevTesting-mongodb-atlas",
-        "url": "mongodb+srv://robsonjesus908:hWkMI3UfU9ihS3KH@devtesting.8ko1ona.mongodb.net/?appName=mongosh+1.10.6"
-    },
-    {
-        "nome": "development-mongodb-atlas",
-        "url": "mongodb+srv://robsonjesus908:hWkMI3UfU9ihS3KH@devtesting.8ko1ona.mongodb.net/?appName=mongosh+1.10.6"
-    },
-    {
-        "nome": "local-mongodb-atlas",
-        "url": "mongodb://localhost:27017/"
-    }
-]
+  {
+    nome: "DevTesting-mongodb-atlas",
+    url: "mongodb+srv://robsonjesus908:hWkMI3UfU9ihS3KH@devtesting.8ko1ona.mongodb.net/?appName=mongosh+1.10.6",
+  },
+  {
+    nome: "development-mongodb-atlas",
+    url: "mongodb+srv://robsonjesus908:hWkMI3UfU9ihS3KH@devtesting.8ko1ona.mongodb.net/?appName=mongosh+1.10.6",
+  },
+  {
+    nome: "local-mongodb-atlas",
+    url: "mongodb://localhost:27017/",
+  },
+];
 
 global.context = {
-    // whichever global context methods you want to mock.
-    // 'services', 'functions', values, etc.
-    app: {
-        "id": realm_config.app_id,
-        "clientAppId": realm_config.app_id,
-        "name": realm_config.name,
-        "projectId": "string",
-        "deployment": {
-            "model": realm_config.deployment_model,
-            "providerRegion": realm_config.provider_region,
-        }
+  // whichever global context methods you want to mock.
+  // 'services', 'functions', values, etc.
+  app: {
+    id: realm_config.app_id,
+    clientAppId: realm_config.app_id,
+    name: realm_config.name,
+    projectId: "string",
+    deployment: {
+      model: realm_config.deployment_model,
+      providerRegion: realm_config.provider_region,
     },
-    environment: {
-        tag: env,
-        values: values
+  },
+  environment: {
+    tag: _env,
+    values: values,
+  },
+  values: {
+    get: (valueName) => {
+      const valueObj = require(`./values/${valueName}`);
+
+      return valueObj.value;
     },
-    values: {
-        get: (valueName) => {
-            const valueObj = require(`./values/${valueName}`);
+  },
+  functions,
+  services: {
+    get: (serviceName) => {
+      let client = _.find(services, function (service) {
+        return service.nome === serviceName;
+      });
 
-            return valueObj.value;
-        }
+      const mongoClient = new MongoClient(client.url);
+
+      return mongoClient;
     },
-    functions,
-    services: {
-        get: (serviceName) => {
-            let client = _.find(services, function(service) {
-                return service.nome === serviceName;
-            });
+  },
+};
 
-            const mongoClient = new MongoClient(client.url);   
-
-            return mongoClient;
-        }
-    }
-}
-
-global.context.functions.tshirt({ name: "t" });
-
-console.log("test");
+execute(_fn, ...args).then(() => {
+  setTimeout(function () {
+    return process.exit(22);
+  }, 5000);
+});
